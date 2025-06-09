edd4ed2c3a3256a28891c15b2d61658be79141eb
Charon <charon.s.tom@gmail.com>
2025-06-09 12:42:17 +0200

    feat(test):testing the homebanner components

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   proxy: {
-  //     // Proxy pour toutes les requêtes /assets → ton back sur le port 3000
-  //     "/assets": {
-  //       target: "http://localhost:3000",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
 });
